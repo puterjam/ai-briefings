77bc5e824bc83532c684cec857cadd35950609c2
PuterJam <hi.puterjam@gmail.com>
2026-02-24 18:31:11 +0800

    Fix charset issues in HTML generation
    - Add http-equiv Content-Type meta tag
    - Add @charset UTF-8 to CSS
    - Add X-UA-Compatible meta for IE

--- scripts/md_to_html.py
+++ scripts/md_to_html.py
@@ -12,9 +12,12 @@ HTML_TEMPLATE = '''<!DOCTYPE html>
 <html lang="zh-CN">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{title}</title>
     <style>
+        @charset "UTF-8";
         :root {{
             --primary: #0066cc;
             --primary-dark: #0052a3;
